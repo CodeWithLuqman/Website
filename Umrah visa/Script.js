3c03783aa0a1cb41b566b122023e7eabd8ea3bca
CodeWithLuqman <mr.luqman091@gmail.com>
2025-05-15 03:06:32 -0700

    Delete Umrah visa directory

--- Umrah visa/Script.js
+++
@@ -1,7 +0,0 @@
-const scrollBtn = document.getElementById("scrollTopBtn");
-    window.onscroll = function () {
-      scrollBtn.style.display = window.scrollY > 300 ? "block" : "none";
-    };
-    scrollBtn.onclick = function () {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    };
