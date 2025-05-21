fdafb703413035653fb0d078e27ebd5485515eb8
CodeWithLuqman <mr.luqman091@gmail.com>
2025-05-21 11:33:32 +0500

    Add files via upload

---
+++ Tourist and buissness Visa/Script1.js
@@ -0,0 +1,23 @@
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((acc) => {
+  acc.addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+});
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+    window.onscroll = function () {
+      scrollBtn.style.display = window.scrollY > 300 ? "block" : "none";
+    };
+    scrollBtn.onclick = function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
